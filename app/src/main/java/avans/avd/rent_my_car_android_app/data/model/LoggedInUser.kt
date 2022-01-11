92f2251a465c125ebb24cb36e783f80a2806acd2
BartWijnja <bart@wijnja.com>
2022-01-11 19:19:19 +0100

    Initial commit

---
+++ app/src/main/java/avans/avd/rent_my_car_android_app/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package avans.avd.rent_my_car_android_app.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
