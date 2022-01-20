248c6dbf9df4a2892e5421803ebf4ef97c968cdb
BartWijnja <bart@wijnja.com>
2022-01-20 21:07:48 +0100

    added CarCategory fragment. Homepage needs work on Constraints

---
+++ app/src/main/java/avans/avd/rent_my_car_android_app/fragment/CarCategoryFragment.kt
@@ -0,0 +1,12 @@
+package avans.avd.rent_my_car_android_app.fragment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import avans.avd.rent_my_car_android_app.R
+
+class CarCategoryFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}
