1b907e866f68efaa1920f457ae805ea2015c7c50
Ivan <i.ivanov.peev@hotmail.com>
2022-02-20 02:44:28 +0100

    added login fragment with validation and JWT

---
+++ app/src/main/java/avans/avd/rent_my_car_android_app/util/Result.kt
@@ -0,0 +1,14 @@
+package avans.avd.rent_my_car_android_app.util
+
+sealed class Result<out T : Any> {
+
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[exception=$exception]"
+        }
+    }
+}
